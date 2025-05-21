017d6c47a77a49f83f10c86d1903b2f53af7ef5c
SlowSnakPierre <38976602+SlowSnakPierre@users.noreply.github.com>
2025-05-21 10:49:40 +0200

    feat: update ESLint configuration and enhance global styles

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+const compat = new FlatCompat({
+	baseDirectory: import.meta.dirname,
+});
+
+export default tseslint.config(
+	{
+		ignores: [".next"],
+	},
+	...compat.extends("next/core-web-vitals"),
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		extends: [...tseslint.configs.recommended],
+		rules: {
+			"@typescript-eslint/array-type": "off",
+			"@typescript-eslint/consistent-type-definitions": "off",
+			"@typescript-eslint/consistent-type-imports": [
+				"warn",
+				{ prefer: "type-imports", fixStyle: "inline-type-imports" },
+			],
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{ argsIgnorePattern: "^_" },
+			],
+			"@typescript-eslint/require-await": "off",
+			"@typescript-eslint/no-misused-promises": [
+				"error",
+				{ checksVoidReturn: { attributes: false } },
+			],
+		},
+	},
+	prettierConfig,
+	{
+		linterOptions: {
+			reportUnusedDisableDirectives: true,
+		},
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+			},
+		},
+	}
+);
